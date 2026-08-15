e003f59ce005a5f4726e272f167622de165d033b
平生三郎 <heizeisaburou@gmail.com>
2026-08-16 01:39:57 +0200

    save

--- lua/user/cfg.lua
+++ lua/user/cfg.lua
@@ -8,7 +8,7 @@ local map = vim.keymap.set
 
 -- Recomiendo desactivar la sincronización del clipboard del sistema y aprender a sincronizar
 -- el cliboard manualmente, a menos que sean tus primeros meses utilizando Neovim.
-local sync_clipboard = false
+local sync_clipboard = true
 if sync_clipboard and vim.fn.has "clipboard" == 1 then
   sabunv.util.clipboard.sync(true)
 end
